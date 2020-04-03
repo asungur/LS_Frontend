101b9133c1872ceefb8a936971651c11013f4d1f
asungur <sunguralican@gmail.com>
2020-04-03 13:11:05 +0100

    finished loops

--- js210/L1_javascript_basics/introduction_to_javascript_book/09_loops.js
+++ js210/L1_javascript_basics/introduction_to_javascript_book/09_loops.js
@@ -37,3 +37,78 @@ while (index < names.length) {
 }
 
 console.log(upperNames); // ['CHRIS', 'KEVIN', 'NAVEED', 'PETE', 'VICTOR']
+
+// DO WHILE LOOP
+let answer;
+do {
+  answer = prompt("Do you want to do that again?");
+} while (answer === 'y');
+
+
+// USING CONTINUE TO SKIP THE REST OF THE BLOCK
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+let upperNames = [];
+
+for (let index = 0; index < names.length; index += 1) {
+  if (names[index] === 'Naveed') {
+    continue;
+  }
+
+  let upperCaseName = names[index].toUpperCase();
+  upperNames.push(upperCaseName);
+}
+
+console.log(upperNames); // ['CHRIS', 'KEVIN', 'PETE', 'VICTOR']
+
+// USING BREAK TO SKIP REST OF THE ITERATIONS
+let array = [3, 1, 5, 9, 2, 6, 4, 7]
+let indexOfFive = -1;
+
+for (let i = 0; i < array.length; i += 1) {
+  if (array[i] === 5) {
+    indexOfFive = i;
+    break;
+  }
+}
+
+console.log(indexOfFive);
+
+// ARRAY ITERATION
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+
+names.forEach(function (name) {
+  console.log(name);
+});
+
+// SINGLE LINER ARRAY ITER
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+
+names.forEach(name => console.log(name));
+
+// RECURSION
+function doubler(number) {
+  console.log(number);
+
+  if (number <= 50) {
+    doubler(number * 2);
+  }
+}
+
+doubler(5);
+// logs
+// 5
+// 10
+// 20
+// 40
+// 80
+
+// RECURSION MORE COMPLEX FIBONACCI EXAMPLE
+function fibonacci(number) {
+  if (number < 2) {
+    return number;
+  } else {
+    return fibonacci(number - 1) + fibonacci(number - 2);
+  }
+}
+
+console.log(fibonacci(6)); // the 6th Fibonacci number is 8
